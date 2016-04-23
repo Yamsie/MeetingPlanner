3fafeb96bd1772b8c1c3b757540476df8146dab5
aoifewatson <aoife.watson@gmail.com>
2016-04-23 16:44:58 +0100

    contract class for db - sets put layout basically

---
+++ app/src/main/java/com/example/james/meetingplanner/DBContract.java
@@ -0,0 +1,28 @@
+package com.example.james.meetingplanner;
+
+import android.provider.BaseColumns;
+
+public class DBContract {
+    public static final String DB_NAME = "meetings_planner";
+    public static final int DB_VERSION = 1;
+
+    private DBContract() {} //private constructor so it cannot be called accidentally
+
+    public class EntryToTableFriends implements BaseColumns {
+        public static final String TABLE = "friends";
+        public static final String COL_TASK_TITLE = "name";
+    }
+
+    public class EntryToTableLocations implements BaseColumns {
+        public static final String TABLE = "locations";
+        public static final String COL_TASK_TITLE = "places";
+    }
+
+    public class EntryToTableMeetings implements BaseColumns {
+        public static final String TABLE = "meetings";
+        public static final String COL1 = "friend";
+        public static final String COL2 = "location";
+        public static final String COL3 = "time";
+        public static final String COL4 = "date";
+    }
+}
