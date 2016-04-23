1d787ea59b26523a5a97d8c2e9f9c22bf8d03579
Yamsie <14147009@studentmail.ul.ie>
2016-04-23 13:44:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/james/meetingplanner/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.james.meetingplanner;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.FragmentManager;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.Contacts;
+import android.provider.ContactsContract;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private final int PICK_CONTACT = 1;
+
+    public void callContacts(View v) {
+
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT);
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+        if(reqCode == PICK_CONTACT) {
+            if(resultCode == AppCompatActivity.RESULT_OK) {
+                Uri contactData = data.getData();
+                Cursor c = getContentResolver().query(contactData, null, null, null, null);
+
+                if(c.moveToFirst()) {
+                    String name = c.getString(c.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
+                    EditText et = (EditText)findViewById(R.id.editCon);
+                    et.setText(name);
+                }
+            }
+        }
+
+    }
+
+    /*
+   public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.buttonTime:
+                FragmentManager fm = this.getFragmentManager();
+                DialogFragment newFragment = new TimePickerFragment();
+                newFragment.show(fm, "timePicker");
+        }
+    }
+    */
+
+}
+
+
