06660ae0d3bcd4bda2c2cfae1785118b55d3d18c
Yamsie <14147009@studentmail.ul.ie>
2016-04-24 17:45:58 +0100

    Verifaction for all fields

--- app/src/main/java/com/example/james/meetingplanner/MainActivity.java
+++ app/src/main/java/com/example/james/meetingplanner/MainActivity.java
@@ -5,6 +5,8 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.FragmentManager;
 import android.app.TimePickerDialog;
+import android.content.ContentResolver;
+import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -79,13 +81,40 @@ public class MainActivity extends AppCompatActivity {
             break;
 
             case R.id.buttonSubmit:
-                if(dateFragment != null && timeFragment != null) {
+                EditText en = (EditText) findViewById(R.id.editCon);
+                EditText el = (EditText) findViewById(R.id.editLoc);
+                EditText ea = (EditText) findViewById(R.id.editAct);
+                if(dateFragment != null && timeFragment != null && isValid(en) && isValid(el) && isValid(ea)) {
                     TextView ed = (TextView) findViewById(R.id.editDate);
                     TextView et = (TextView) findViewById(R.id.editTime);
                     if(compareWithCurrentDate(ed.getText().toString(), et.getText().toString())) {
                         Toast.makeText(this, "Date parsed and returned!", Toast.LENGTH_SHORT).show();
+
+                        try {
+                            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+                            Date inDate = dateFormat.parse(ed.getText().toString() + " " + et.getText().toString());
+                            Calendar cal = Calendar.getInstance();
+                            cal.setTime(inDate);
+
+                            Intent calintent = new Intent(Intent.ACTION_EDIT);
+                            calintent.setType("vnd.android.cursor.item/event");
+                            calintent.putExtra("beginTime", cal.getTimeInMillis());
+                            calintent.putExtra("allDay", false);
+                            calintent.putExtra("duration", "PT.5H");
+                            calintent.putExtra("title", "Meeting with " + en.getText().toString() + "!");
+                            startActivity(calintent);
+                        } catch (ParseException e) {
+                            Toast.makeText(this, "Unexpected error", Toast.LENGTH_SHORT).show();
+                        }
+
+                    } else {
+                        Toast.makeText(this, "Select a later time!", Toast.LENGTH_SHORT).show();
                     }
 
+                } else if (dateFragment == null && timeFragment == null) {
+                    Toast.makeText(this, "Select a time and date!", Toast.LENGTH_SHORT).show();
+                } else if (!isValid(en) || !isValid(el) || !isValid(ea)) {
+                    Toast.makeText(this, "Please fill in all fields with alphanumerical characters only!", Toast.LENGTH_SHORT).show();
                 }
                 else Toast.makeText(this, "Select a time and date!", Toast.LENGTH_SHORT).show();
 
@@ -110,10 +139,17 @@ public class MainActivity extends AppCompatActivity {
 
         if(cal.compareTo(today) > 0) {
             returnValue = true;
-        } else {
-            Toast.makeText(this,  "Pick a later time!", Toast.LENGTH_SHORT).show();
         }
 
+        return returnValue;
+    }
+
+    private boolean isValid(EditText etText) {
+        boolean returnValue = false;
+        String regex = "[A-Za-z0-9]+";
+        if(etText.getText().toString().matches(regex) && etText.getText().toString().trim().length() > 0) {
+            returnValue = true;
+        }
         return returnValue;
     }
 }
