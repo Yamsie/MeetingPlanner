3f027a6ede7a30ba170ddef17953d40ff07d107c
Yamsie <14147009@studentmail.ul.ie>
2016-04-24 14:02:56 +0100

    Time and date verification. Tidied EditText boxes (take single line only). Removed unnecessary code.

--- app/src/main/java/com/example/james/meetingplanner/MainActivity.java
+++ app/src/main/java/com/example/james/meetingplanner/MainActivity.java
@@ -17,13 +17,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private final int PICK_CONTACT = 1;
+    private DialogFragment dateFragment;
+    private DialogFragment timeFragment;
 
 
     @Override
@@ -56,19 +65,56 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
                 startActivityForResult(intent, PICK_CONTACT);
             break;
+
             case R.id.buttonTime:
                 FragmentManager fmTime = this.getFragmentManager();
-                DialogFragment timeFragment = new TimePickerFragment(this);
+                timeFragment = new TimePickerFragment(this);
                 timeFragment.show(fmTime, "timePicker");
             break;
 
             case R.id.buttonDate:
                 FragmentManager fmDate = this.getFragmentManager();
-                DialogFragment dateFragment = new DatePickerFragment(this);
+                dateFragment = new DatePickerFragment(this);
                 dateFragment.show(fmDate, "datePicker");
             break;
 
+            case R.id.buttonSubmit:
+                if(dateFragment != null && timeFragment != null) {
+                    TextView ed = (TextView) findViewById(R.id.editDate);
+                    TextView et = (TextView) findViewById(R.id.editTime);
+                    if(compareWithCurrentDate(ed.getText().toString(), et.getText().toString())) {
+                        Toast.makeText(this, "Date parsed and returned!", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                else Toast.makeText(this, "Select a time and date!", Toast.LENGTH_SHORT).show();
+
+            break;
+
         }
+    }
+
+    private boolean compareWithCurrentDate(String date, String time)  {
+        boolean returnValue = false;
+        String fullDate = date + " " + time;
+        Calendar cal = Calendar.getInstance();
+        Calendar today = Calendar.getInstance();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+        try {
+            cal.setTime(dateFormat.parse(fullDate));
+            dateFormat.format(today.getTime());
+        } catch(ParseException e) {
+            Toast.makeText(this, "Date unable to parse!", Toast.LENGTH_SHORT).show();
+        }
+
+        if(cal.compareTo(today) > 0) {
+            returnValue = true;
+        } else {
+            Toast.makeText(this,  "Pick a later time!", Toast.LENGTH_SHORT).show();
+        }
+
+        return returnValue;
     }
 }
 
