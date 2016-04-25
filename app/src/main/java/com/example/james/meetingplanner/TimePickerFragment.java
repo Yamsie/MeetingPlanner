b2c582ca7a14597b40db9d9668dc8dd5c353a614
Yamsie <14147009@studentmail.ul.ie>
2016-04-25 14:37:44 +0100

    Test12

---
+++ app/src/main/java/com/example/james/meetingplanner/TimePickerFragment.java
@@ -0,0 +1,44 @@
+package com.example.james.meetingplanner;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import java.util.Calendar;
+
+
+public class TimePickerFragment extends DialogFragment
+        implements TimePickerDialog.OnTimeSetListener {
+
+    public Activity activity;
+
+    TimePickerFragment(Activity inActivity) {
+        activity = inActivity;
+    }
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the current time as the default values for the picker
+        final Calendar c = Calendar.getInstance();
+        int hour = c.get(Calendar.HOUR_OF_DAY);
+        int minute = c.get(Calendar.MINUTE);
+
+        // Create a new instance of TimePickerDialog and return it
+        return new TimePickerDialog(getActivity(), this, hour, minute,
+                DateFormat.is24HourFormat(getActivity()));
+    }
+
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+            String zero = "";
+            if(minute < 10)
+                zero = "0";
+            String selTime = hourOfDay + ":" + zero + minute;
+            TextView et = (TextView) activity.findViewById(R.id.editTime);
+            et.setText(selTime);
+    }
+}
