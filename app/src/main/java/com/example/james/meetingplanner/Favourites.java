b1651b38ebef4999af1a1b4b8df4502bc36fecfa
Yamsie <14147009@studentmail.ul.ie>
2016-04-28 12:17:53 +0100

    Commented in what the functionality of each button in Favourites.java should be.

--- app/src/main/java/com/example/james/meetingplanner/Favourites.java
+++ app/src/main/java/com/example/james/meetingplanner/Favourites.java
@@ -15,6 +15,56 @@ public class Favourites extends AppCompatActivity {
     }
 
     public void onClick(View v) {
-        Toast.makeText(this, "Something happened...", Toast.LENGTH_SHORT).show(); //blah
+        switch(v.getId()) {
+            case R.id.viewFavActs:
+                /*
+                Present list with all of users favourite activities saved to DB.
+                 */
+                break;
+
+            case R.id.editFavActs:
+                /*
+                Take user input from right hand side.
+                - Verify it. Check for one alphanumeric characters
+                  only AND spaces (or [0-9a-zA-Z ]+)
+                - Add it to database.
+                 */
+                break;
+
+            case R.id.delFavActs:
+                /*
+                Take user to list of activities.
+                - Allow them to select one, and present a pop-up that will ask if
+                  they would like to delete it. Have "Yes" and "No" buttons.
+                - Delete selection from the database.
+                 */
+                break;
+
+            case R.id.viewFavLocs:
+                /*
+                Present list with all of users favourite locations saved to DB.
+                 */
+                break;
+
+            case R.id.editFavLocs:
+                /*
+                Take user input from right hand side.
+                - Verify it. Check for one alphanumeric characters
+                  only AND spaces (or [0-9a-zA-Z ]+)
+                - Add it to database.
+                 */
+                break;
+
+            case R.id.delFavLocs:
+                /*
+                Take user to list of locations.
+                - Allow them to select one, and present a pop-up that will ask if
+                they would like to delete it. Have "Yes" and "No" buttons.
+                - Delete selection from the database.
+                 */
+                break;
+
+
+        }
     }
 }
