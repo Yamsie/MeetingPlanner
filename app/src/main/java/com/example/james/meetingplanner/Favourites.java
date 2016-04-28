7f2aed11f225ad678d5f125966c9a7a6bbb3c892
Yamsie <14147009@studentmail.ul.ie>
2016-04-28 11:34:59 +0100

    Tried to add favourites...

---
+++ app/src/main/java/com/example/james/meetingplanner/Favourites.java
@@ -0,0 +1,20 @@
+package com.example.james.meetingplanner;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class Favourites extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favourites);
+    }
+
+    public void onClick(View v) {
+        Toast.makeText(this, "Something happened...", Toast.LENGTH_SHORT).show();
+    }
+}
