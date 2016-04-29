e1ad2a0898b35a349f0ac9fb91542d03f733390e
Paddy15crge <14167387@studentmail.ul.ie>
2016-04-29 12:35:31 +0100

    music on button push

--- app/src/main/java/com/example/james/meetingplanner/Menu.java
+++ app/src/main/java/com/example/james/meetingplanner/Menu.java
@@ -14,6 +14,7 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
     }
 
     public void onClick(View v) {
